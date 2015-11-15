1e131659af22950125a5e912de157962a5b0cda1
Wes Eklund <s.wes35@gmail.com>
2015-11-15 13:29:50 -0500

    new routes for about and demo page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
+var express = require('express');
+var routes = express.Router();
+
 exports.index = function(req, res){
-// console.log("search input");
   res.render('index');
 };
+
+exports.demo = function(req,res){
+  res.render('demo');
+}
+
+exports.about = function(req,res){
+  res.render('about');
+}
